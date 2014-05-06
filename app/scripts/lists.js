37a7d4ec9022edb3c0c2c6f670346c695b74db7c
brookebc <brooke.casey@gmail.com>
2014-05-06 14:24:35 -0400

    duplication of my todoer app to re-factor to get, post, delete and update using ajax

---
+++ app/scripts/lists.js
@@ -0,0 +1,14 @@
+// placeholder array for objects to be added
+
+var activetodoarray = [
+{
+	activity: "Make this super todo app"
+	},
+
+{
+	activity: "Go to the beach"
+	}
+
+];
+
+// var newtoarray = [];
