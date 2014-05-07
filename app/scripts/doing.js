332e4987a8d5096b7277842eaee7aebb15d3821e
brookebc <brooke.casey@gmail.com>
2014-05-07 17:15:05 -0400

    i can post get and delete from the server thats right

--- app/scripts/doing.js
+++ app/scripts/doing.js
@@ -17,6 +17,7 @@ initStyling: function() {
 },
 initEvents: function(){
 	$("form").on("submit",this.addToDos);
+	$(".list").on("click",".removeToDo", this.removeMe);
 },
 
 renderToDos: function(){
@@ -24,7 +25,7 @@ renderToDos: function(){
 	$.ajax({
 			type:"GET",
 			url: "http://tiy-fee-rest.herokuapp.com/collections/brookebc",
-			datatype: "json",
+			datatype: "JSON",
 			error: function(jqXHR, status, error){
 				alert("no! it is not posting this to the page.");
 			},
@@ -35,37 +36,29 @@ renderToDos: function(){
 				var html = '';
 
 				for (var i = 0; i <todos.length; i++){
-					// var obj = data[i].data;
-
-					// console.log(data);
 					
-					// var name = name;
-					// var name = name;
-					
-				html += '<li data-postId=' + todos[i]._id + '>' + todos[i].name + '<span class="glyphicon glyphicon-remove removeToDo"></span></li>';
-				}; var postId = $(this).closest("article").data("postid");
+				html += '<li data-postId=' + todos[i]._id + '>' + todos[i].name + '<button class="glyphicon glyphicon-remove removeToDo" id="removeToDo"></button></li>';
+				}; 
 
 				console.log("yeah it is working down here- about to add all the todos to the page");
 				$("#righthere").html(html);
 			}
 		});
-	},
+},
 
 
 addToDos: function(e) {
     e.preventDefault();
-    // var data = data;
 
     var newToDo = {
               name: $("#newitemtoadd").val(),
-              // _.id: 
         };
 
 	$.ajax({
 		type:"POST",
 		url: "http://tiy-fee-rest.herokuapp.com/collections/brookebc",
 		data: newToDo,
-		datatype: "json",
+		datatype: "JSON",
 		error: function(jqXHR, status, error){
 			alert("no! there is an error.");
 		},
@@ -75,30 +68,36 @@ addToDos: function(e) {
 			$("#newitemtoadd").val(),
 
 			console.log(newToDo);
-
         	ToDos.renderToDos(data);
 		}
 	});
 
+},
+
+removeMe: function(e) {
+	e.preventDefault();
+
+	console.log("I heard you click in an li to REMOVE something");
+
+    var $thisToDO = $(this).closest("li");
+    console.log($thisToDO);
+    var postId = $thisToDO.data("postid");
+    console.log(postId);
+    
+    $.ajax({
+    type: "DELETE",
+    url: "http://tiy-fee-rest.herokuapp.com/collections/brookebc/" + postId,
+    error: function(jqXHR, status, error) {
+        alert("error couldnt delete that thing");
+    }, 
+    success: function(data) {
+      	alert("yes! got rid of that todo");
+        ToDos.renderToDos(data);  
+
+      }
+    });
 }
 
- // removeItem: function() {
- //    var $thisPost = $(this).closest("article")
- //    var postId = $thisPost.data("postid");
-
- //    $.ajax({
- //      url: "http://tiy-fee-rest.herokuapp.com/collections/myBlog/" + postId,
- //      type: "DELETE",
- //      error: function(jqXHR, status, error) {
- //        alert("couldnt delete that thing");
- //      }, 
- //      success: function(data) {
- //      	alert("yes! got rid of that todo");
- //         myBlog.renderPosts();  
-
- //      }
- //    });
- //  }
 
 };	
 	
